43f1996525e1c30b18a9ff072f45e92e09efc080
Brendon Silva <brendon.dasilva03@gmail.com>
2022-07-04 15:39:26 -0300

    first commit

--- README.md
+++ README.md
@@ -1,46 +1,39 @@
-# Getting Started with Create React App
+# Front-End Teste - DoroTech
 
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+Neste projeto temos: dark/light mode, buscar personagens pelo nome, paginação e filtrar personagens por status e/ou espécie. 
+Sobre paginação, busca de personagens e filtros: foram definidas variáveis, onde os valores são inseridos à url da API. Fazendo com que o useEffect execute uma ação a cada modificação da url na API conferindo se há resultados.
 
-## Available Scripts
+repositório: https://github.com/BrendonSSilva/ReactJsProject-API-RickAndMorty.git
 
-In the project directory, you can run:
+link hospedado no firebase: https://appmortyandrick.web.app
 
-### `npm start`
+# Tecnologias Utilizadas
+- ReactJs
+- TypeScript
+- Sass
 
-Runs the app in the development mode.\
-Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
+# Como Utilizar?
 
-The page will reload if you make edits.\
-You will also see any lint errors in the console.
+~~~
+- Clonar o repositório:
+$ git clone https://github.com/BrendonSSilva/ReactJsProject-API-RickAndMorty.git
 
-### `npm test`
+- Entrar no diretório
+- Instalar as dependências:
+$ npm install axios
 
-Launches the test runner in the interactive watch mode.\
-See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
+$ npm install node-sass --save
 
-### `npm run build`
+- Iniciar o app: 
+$ npm start
+~~~
 
-Builds the app for production to the `build` folder.\
-It correctly bundles React in production mode and optimizes the build for the best performance.
+Link no Firebase: https://appmortyandrick.web.app
 
-The build is minified and the filenames include the hashes.\
-Your app is ready to be deployed!
+Repositório: https://github.com/BrendonSSilva/ReactJsProject-API-RickAndMorty.git
 
-See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
+# Autor
+Brendon Schimmelpfennig da Silva
 
-### `npm run eject`
-
-**Note: this is a one-way operation. Once you `eject`, you can’t go back!**
-
-If you aren’t satisfied with the build tool and configuration choices, you can `eject` at any time. This command will remove the single build dependency from your project.
-
-Instead, it will copy all the configuration files and the transitive dependencies (webpack, Babel, ESLint, etc) right into your project so you have full control over them. All of the commands except `eject` will still work, but they will point to the copied scripts so you can tweak them. At this point you’re on your own.
-
-You don’t have to ever use `eject`. The curated feature set is suitable for small and middle deployments, and you shouldn’t feel obligated to use this feature. However we understand that this tool wouldn’t be useful if you couldn’t customize it when you are ready for it.
-
-## Learn More
-
-You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
-
-To learn React, check out the [React documentation](https://reactjs.org/).
+GitHub: https://github.com/BrendonSSilva
+LinkedIn: https://www.linkedin.com/in/brendon-silva/
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,145 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import s from './App.module.scss';
+import './App.module.scss';
+import axios from 'axios'
 
-function App() {
+const App = () => {
+
+  //buscar API
+  const [data, setData] = useState({ results: [] })
+
+  const characters = async () => {
+    const result = await axios(`https://rickandmortyapi.com/api/character/?page=${page}&name=${theName}&status=${stt}&species=${specie}`)
+    setData(result.data)
+  }
+
+  //pages
+  const [page, setPages] = useState(1)
+
+  const pageUp = () => {
+    page < 43 ? setPages(page + 1) : setPages(42)
+  }
+  const pageDown = () => {
+    page > 1 ? setPages(page - 1) : setPages(1)
+  }
+
+  //darkMode
+  const [dark, setDark] = useState(true)
+
+  const darkMode = () => {
+    dark === false ? setDark(true) : setDark(false)
+  }
+
+  //pesquisar
+  const [theName, setTheName] = useState('')
+  const [search, setSearch] = useState('')
+
+  const searchChar = (e: any) => {
+    e.preventDefault()
+    setSearch(theName)
+  }
+
+  //filters
+
+  //status
+  const [stt, setStt] = useState('')
+
+  const clickAlive = () => {
+    setStt('Alive')
+  };
+  const clickDead = () => {
+    setStt('Dead')
+  }
+  const clickUnknown = () => {
+    setStt('Unknown')
+  }
+  const clickReset = () => {
+    setStt('')
+  }
+
+  //espécie
+  const [specie, setSpecie] = useState('')
+
+  const clickHuman = () => {
+    setSpecie('Human')
+  }
+  const clickAlien = () => {
+    setSpecie('Alien')
+  }
+  const clickAnimal = () => {
+    setSpecie('Animal')
+  }
+  const clickSpecieReset = () => {
+    setSpecie('')
+  }
+
+  useEffect(
+    () => {
+      characters()
+    }, [search, stt, specie, page])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={dark === false ? s.light : s.dark}>
+      <div className={s.filters}>
+        {/* pesquisar */}
+        <form onSubmit={searchChar}>
+          <button type="button" onClick={darkMode} className={s.darkBtn}>{dark === true ? 'Light Mode' : 'Dark Mode'}</button>
+          <div>
+            <input type="text" value={theName}
+              placeholder='Search character'
+              onChange={e => setTheName(e.target.value)}
+              className={dark === false ? s.light : s.dark} />
+            <button type="submit" className={s.btnSearch}>Search</button>
+          </div>
+        </form>
+
+        {/* status dos personagens */}
+        <div className={s.stt}>
+          <button type="submit" onClick={clickAlive}>Alive</button>
+          <button type="submit" onClick={clickDead}>Dead</button>
+          <button type="submit" onClick={clickUnknown}>Unknown</button>
+          <button type="submit" onClick={clickReset}>Reset Filters</button>
+        </div>
+
+        {/* espécies dos personagens */}
+        <div className={s.species}>
+          <button type="submit" onClick={clickHuman}>Human</button>
+          <button type="submit" onClick={clickAlien}>Alien</button>
+          <button type="submit" onClick={clickAnimal}>Animal</button>
+          <button type="submit" onClick={clickSpecieReset}>Reset Filters</button>
+        </div>
+        <div className={s.sttSpc}>
+          <p>Status: <strong>{stt}</strong></p>
+          <p>Specie: <strong>{specie}</strong></p>
+        </div>
+      </div>
+
+      {/* cards */}
+      <div className={s.card}>
+        <div className={s.pages}>
+          <button type="button" className={s.btnNext} onClick={pageDown}> Previous</button>
+          <button type="button" className={s.btnNext} onClick={pageUp}>Next</button>
+        </div>
+
+        <div className={s.dados}>
+          <div>{data.results.map((item: any) => (
+            <li key={item.id}>
+              <img alt={item.name} src={item.image} />
+              <span><small>Name:</small> {item.name}</span>
+              <span><small>Status:</small> {item.status}</span>
+              {/* detalhes extras dos personagens */}
+              <details>
+                <summary>Detalhes</summary>
+                <span><small>Specie:</small> {[item.species]}</span><br />
+                <span><small>Gender:</small> {item.gender}</span>
+              </details>
+            </li>))}
+          </div>
+        </div>
+      </div>
+      <footer className={dark === false ? s.light : s.dark} >Projeto (desafio) feito por <a href='https://github.com/BrendonSSilva'>Brendon</a></footer>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
